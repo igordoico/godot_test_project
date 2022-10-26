4074b94d6abfbb619d70dff28949bf9200732d82
Igor Doico <igor.doico@gmail.com>
2022-10-26 19:38:51 -0300

    first

--- Player.gd
+++ Player.gd
@@ -41,4 +41,12 @@ func _process(delta):
 
 
 func _on_Player_body_entered(body):
-	pass # Replace with function body.
+	hide() # Player disappears after being hit.
+	emit_signal("hit")
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
